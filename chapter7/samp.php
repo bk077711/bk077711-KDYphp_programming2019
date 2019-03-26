0f1ffd8e99068a5121b60420f6283ec683e6b2ce
bk077711 <bk077711@naver.com>
2019-03-26 16:47:29 +0900

    asdf

---
+++ chapter7/samp.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2019-03-12
+ * Time: ¿ÀÈÄ 4:33
+ */
+$a = 50000;
+echo $a;
+echo '<br>';
+echo "Hello World<br>";
+echo "°ýµµ³×³Û¶í {$a}";
+echo --$a;
+echo $a++;
+echo $a;
+?>
